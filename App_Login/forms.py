f6be0577df0ed47c9a8ed0e6ceaf1f36fe045e15
touhidulislamkhan <touhidikhan0@gmail.com>
2021-04-10 14:02:45 +0600

    Login app-complete login, logout,authentication

---
+++ App_Login/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from App_Login.models import UserProfile
+
+
+class CreateNewUser(UserCreationForm):
+    email = forms.EmailField(required=True, label="Email", widget=forms.TextInput(
+        attrs={'placeholder': 'Email'}))
+    username = forms.CharField(required=True, label="Username", widget=forms.TextInput(
+        attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(
+        required=True,
+        label="Password",
+        widget=forms.PasswordInput(attrs={'placeholder': "Password"})
+    )
+    password2 = forms.CharField(
+        required=True,
+        label="Confirm Password",
+        widget=forms.PasswordInput(
+            attrs={'placeholder': "Password Confirmation"})
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password1', 'password2')
+
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
